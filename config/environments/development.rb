ddb438acc18b4354f4a34234dcd1825e50630299
Mitt Coats <mittcoats@gmail.com>
2016-03-16 10:54:56 -0400

    Implement gem and config files for email in dev and prod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  # Send emails with letter opener in dev mode to open emails in browser
+  config.action_mailer.delivery_method = :letter_openers
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
